77fa4b633888724451811b207172a231f379e079
SangDoo Nam <kexe009@hotmail.com>
2023-08-22 15:55:26 +0200

    add assignment_operators.py

---
+++ assignment_operators.py
@@ -0,0 +1,123 @@
+"""Task 1 : Write a Python script that takes two integers 
+and assigns their sum to a variable using the "+=" operator. 
+Print the value of the variable."""
+
+# a = 10
+# b = 4
+
+# a += b
+
+# print('The sum is', a)
+
+num1 = 34
+num2 = 87
+
+sum = 0
+
+sum += num1
+sum += num2
+
+print ("This sum is", sum)
+
+
+
+"""Task 2 : Write a Python script that takes multiple integers 
+and assigns the product of all the numbers to a variable using the "*=" operator. 
+Print the value of the variable."""
+
+# x = 55
+# y = 78
+
+# x *= y
+
+# print('The product is: ', x )
+
+num1 = 49
+num2 = 856
+num3 = 58
+num4 = 99
+num5 = 7
+product = 1
+
+product *= num1
+product *= num2
+product *= num3
+product *= num4
+product *= num5
+
+print("The Product is =", product )
+
+
+"""Task 3 : Write a Python script that takes multiple characters 
+and assigns these characters to a variable using the "+=". 
+Print the value of the variable."""
+
+# q = 'What'
+# w = ' is it ?'
+
+# q += w
+
+# print('The resulting string is :', q)
+
+char1 = 'P'
+char2 = 'y'
+char3 = 't'
+char4 = 'h'
+char5 = 'o'
+char6 = 'n'
+char7 = ' '
+char8 = 'i'
+char9 = 's'
+char10 = ' '
+char11 = 'f'
+char12 = 'u'
+char13 = 'n'
+
+reverse = ''
+
+reverse += char13
+reverse += char12
+reverse += char11
+reverse += char10
+reverse += char9
+reverse += char8
+reverse += char7
+reverse += char6
+reverse += char5
+reverse += char4
+reverse += char3
+reverse += char2
+reverse += char1
+
+print("The resulting string is :", reverse)
+
+"""Task 4 : Write a Python script that takes multiple strings assigns 
+the concatenation of all the strings to a variable using the "+=" operator. 
+Print the value of the variable."""
+
+# q = "cup"
+# w = "lamp"
+# e = "book"
+# r = "computer"
+# a = "note"
+# s = "ventilator"
+# d = "monitor"
+# f = "ruler"
+
+# q += w + e + r + a +s + d +f 
+
+# print("The concantenated string is : ", q )
+
+item1 = "cup"
+item2 = "lamp"
+item3 = "book"
+item4 = "computer"
+
+concatenate = ""
+
+concatenate += item1
+concatenate += item2
+concatenate += item3
+concatenate += item4
+
+print("The concatenated string is :", concatenate)
